7705e84aa9f90bf87f1b37e902a937ebe1112c17
M Salman <salman39707@gmail.com>
2025-10-09 23:08:50 +0500

    first

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import styles from './footer.module.css';
-import Link from 'next/link';
+"use client";
 
-const Footer = () => {
+import { useEffect, useState } from "react";
+import './footer.css';
+
+
+export default function Footer() {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    setVisible(scrolled > 200);
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisible);
+    return () => window.removeEventListener("scroll", toggleVisible);
+  }, []);
+
   return (
-    <div className='footer-containet'>
-      <ul className='footerSection'>
-        <li>Copyright © VeduApp.cc All Rights Reserved.</li>
-      </ul>
-      <div className='footerSection'>
-        <p>Contact Us: contact@jigsawplanet.us</p>
-        <p>Phone: +92 336 3891016</p>
+    <footer className="footer-container">
+      <div className="footer-left">
+        <p>Copyright © VeduApp.cc All Rights Reserved.</p>
       </div>
-    </div>
-  );
-};
 
-export default Footer;
+      <div className="footer-right">
+        <a href="#">Policy</a>
+        <span>|</span>
+        <a href="#">Disclaimer</a>
+        <span>|</span>
+        <a href="#">Contact Us</a>
+      </div>
+
+      {visible && (
+        <div className="scroll-top" onClick={scrollToTop}>
+          <span>↑</span>
+        </div>
+      )}
+    </footer>
+  );
+}
