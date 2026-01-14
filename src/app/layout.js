c837a483691770789e5cc4093e718791d27185df
M Salman <salman39707@gmail.com>
2026-01-14 15:23:34 +0500

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        
+        <meta name="google-site-verification" content="jx5k8JHVr336GkYA7eOhf5UVnbtoC_wmbY0Y5CnlVsw" />
         <link rel="icon" type="image/png" href="/favicon.webp" />
       </head>
 
