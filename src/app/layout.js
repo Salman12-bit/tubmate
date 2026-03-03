19862df2c329fd0f14f13532489555752303ebd3
M Salman <salman39707@gmail.com>
2026-03-03 19:34:38 +0500

    first

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,6 +18,18 @@ export default function RootLayout({ children }) {
       </head>
 
       <body className={inter.className}>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-GSGVEDCPCC"
+        />
+
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-GSGVEDCPCC');
+          `}
+        </Script>
 
         <AuthProvider>
           <div className="container">
